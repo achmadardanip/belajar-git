1933fc8cfab6bcacddd212ac8d6711414f214605
Achmad Ardani Prasha <achmadardani7@gmail.com>
2020-08-19 16:10:12 +0700

    perubahan kedua home_crud

--- application/views/home_crud.php
+++ application/views/home_crud.php
@@ -64,8 +64,9 @@
 				<div class="card-header"><strong><?php echo $title ?></strong></div>
 				<div class="card-body">
                     <div id="printbar" style="float:right;"></div>
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                    <p>"test"</p>
+                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>      
+                    
+                    <p>Hello world</p>
                 </div>
 			</div>
 			<!-- </div> -->
